75f543cb34210fc8902a51120947c1e52c93bedc
samiwarraich <samiwarraich51@gmail.com>
2021-06-22 20:00:33 +0500

    initial commit

---
+++ frontend/src/components/admin/ProductsList.js
@@ -0,0 +1,133 @@
+import React, { useEffect } from "react";
+import { MDBDataTable } from "mdbreact";
+import { Link } from "react-router-dom";
+import { useDispatch, useSelector } from "react-redux";
+import { useAlert } from "react-alert";
+import MetaData from "../layout/MetaData";
+import Loader from "../layout/Loader";
+import Sidebar from "./Sidebar";
+import {
+  getAdminProducts,
+  clearErrors,
+  deleteProduct,
+} from "../../actions/productActions";
+import { DELETE_PRODUCT_RESET } from "../../constants/productConstant";
+
+const ProductsList = ({ history }) => {
+  const alert = useAlert();
+  const dispatch = useDispatch();
+
+  const { loading, error, products } = useSelector((state) => state.products);
+  const { error: deleteError, isDeleted } = useSelector(
+    (state) => state.product
+  );
+
+  useEffect(() => {
+    dispatch(getAdminProducts());
+    if (error) {
+      alert.error(error);
+      dispatch(clearErrors());
+    }
+    if (deleteError) {
+      alert.error(deleteError);
+      dispatch(clearErrors());
+    }
+    if (isDeleted) {
+      alert.success("product deleted successfully");
+      history.push("/admin/products");
+      dispatch({ type: DELETE_PRODUCT_RESET });
+    }
+  }, [dispatch, alert, error, deleteError, isDeleted, history]);
+
+  const setProducts = () => {
+    const data = {
+      columns: [
+        {
+          label: "ID",
+          field: "id",
+          sort: "asc",
+        },
+        {
+          label: "Name",
+          field: "name",
+          sort: "asc",
+        },
+        {
+          label: "Price",
+          field: "price",
+          sort: "asc",
+        },
+        {
+          label: "Stock",
+          field: "stock",
+          sort: "asc",
+        },
+        {
+          label: "Actions",
+          field: "actions",
+        },
+      ],
+      rows: [],
+    };
+    products.forEach((product) => {
+      data.rows.push({
+        id: product._id,
+        name: product.name,
+        price: `$${product.price}`,
+        stock: product.stock,
+        actions: (
+          <>
+            <Link
+              to={`/admin/product/${product._id}`}
+              className="btn btn-primary py-1 px-2"
+            >
+              <i className="fa fa-pencil"></i>
+            </Link>
+            <button
+              className="btn btn-danger py-1 px-2 ml-2"
+              onClick={() => deleteProductHandler(product._id)}
+            >
+              <i className="fa fa-trash"></i>
+            </button>
+          </>
+        ),
+      });
+    });
+    return data;
+  };
+
+  const deleteProductHandler = (id) => {
+    dispatch(deleteProduct(id));
+  };
+
+  return (
+    <>
+      <MetaData title={"All Products"} />
+      <div className="row">
+        <div className="col-12 col-md-2">
+          <Sidebar />
+        </div>
+        <div className="col-12 col-md-10">
+          <>
+            <h1 className="my-5">All Products</h1>
+            {loading ? (
+              <Loader />
+            ) : (
+              <>
+                <MDBDataTable
+                  data={setProducts()}
+                  className="px-3"
+                  bordered
+                  striped
+                  hover
+                />
+              </>
+            )}
+          </>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default ProductsList;
---
+++ frontend/src/components/admin/UsersList.js
@@ -0,0 +1,124 @@
+import React, { useEffect } from "react";
+import { MDBDataTable } from "mdbreact";
+import { Link } from "react-router-dom";
+import { useDispatch, useSelector } from "react-redux";
+import { useAlert } from "react-alert";
+import MetaData from "../layout/MetaData";
+import Loader from "../layout/Loader";
+import Sidebar from "./Sidebar";
+import { allUsers, clearErrors, deleteUser } from "../../actions/userActions";
+import { DELETE_USER_RESET } from "../../constants/userConstant";
+
+const UsersList = ({ history }) => {
+  const alert = useAlert();
+  const dispatch = useDispatch();
+
+  const { loading, error, users } = useSelector((state) => state.allUsers);
+  const { isDeleted } = useSelector((state) => state.user);
+
+  useEffect(() => {
+    dispatch(allUsers());
+    if (error) {
+      alert.error(error);
+      dispatch(clearErrors());
+    }
+
+    if (isDeleted) {
+      alert.success("User deleted successfully");
+      history.push("/admin/users");
+      dispatch({ type: DELETE_USER_RESET });
+    }
+  }, [dispatch, alert, error, isDeleted, history]);
+
+  const deleteUserHandler = (id) => {
+    dispatch(deleteUser(id));
+  };
+
+  const setUsers = () => {
+    const data = {
+      columns: [
+        {
+          label: "User ID",
+          field: "id",
+          sort: "asc",
+        },
+        {
+          label: "Name",
+          field: "name",
+          sort: "asc",
+        },
+        {
+          label: "Email",
+          field: "email",
+          sort: "asc",
+        },
+        {
+          label: "Role",
+          field: "role",
+          sort: "asc",
+        },
+        {
+          label: "Actions",
+          field: "actions",
+        },
+      ],
+      rows: [],
+    };
+    users.forEach((user) => {
+      data.rows.push({
+        id: user._id,
+        name: user.name,
+        email: user.email,
+        role: user.role,
+        actions: (
+          <>
+            <Link
+              to={`/admin/user/${user._id}`}
+              className="btn btn-primary py-1 px-2"
+            >
+              <i className="fa fa-pencil"></i>
+            </Link>
+            <button
+              className="btn btn-danger py-1 px-2 ml-2"
+              onClick={() => deleteUserHandler(user._id)}
+            >
+              <i className="fa fa-trash"></i>
+            </button>
+          </>
+        ),
+      });
+    });
+    return data;
+  };
+
+  return (
+    <>
+      <MetaData title={"All Users"} />
+      <div className="row">
+        <div className="col-12 col-md-2">
+          <Sidebar />
+        </div>
+        <div className="col-12 col-md-10">
+          <>
+            <h1 className="my-5">All Users</h1>
+            {loading ? (
+              <Loader />
+            ) : (
+              <>
+                <MDBDataTable
+                  data={setUsers()}
+                  className="px-3"
+                  bordered
+                  striped
+                  hover
+                />
+              </>
+            )}
+          </>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default UsersList;
