ae3d1c04a078da7213fbc156ef2ae86ac137f62c
Sami Warraich <samiwarraich51@gmail.com>
2023-02-28 20:29:03 +0300

    Revert ""
    This reverts commit f220965b00b65eac20e8b2aa091705a378e9e4f1.

--- backend/app.js
+++ backend/app.js
@@ -11,8 +11,6 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 app.use(fileUpload());
-app.use(express.static(path.join(__dirname, "../frontend/build")));
-app.use(express.static(path.join(__dirname, "../frontend/public")));
 
 const products = require("./routes/product");
 const auth = require("./routes/auth");
@@ -25,6 +23,9 @@ app.use("/api/v1", payment);
 app.use("/api/v1", order);
 
 if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../frontend/build")));
+  app.use(express.static(path.join(__dirname, "../frontend/public")));
+
   app.get("*", (req, res) => {
     res.sendFile(path.resolve(__dirname, "../frontend/build/index.html"));
   });
