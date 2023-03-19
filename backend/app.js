10ba2c3d3c9e5f7d5ed666f86d5c0969d8f3b32b
Sami Warraich <samiwarraich51@gmail.com>
2023-03-19 18:50:43 +0300

    fixes

--- backend/app.js
+++ backend/app.js
@@ -22,12 +22,12 @@ app.use("/api/v1", auth);
 app.use("/api/v1", payment);
 app.use("/api/v1", order);
 
-if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, "../frontend/build")));
-  app.get("*", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "../frontend/build/index.html"));
-  });
-}
+// if (process.env.NODE_ENV === "production") {
+//   app.use(express.static(path.join(__dirname, "../frontend/build")));
+//   app.get("*", (req, res) => {
+//     res.sendFile(path.resolve(__dirname, "../frontend/build/index.html"));
+//   });
+// }
 
 app.use(errorMiddleware);
 
