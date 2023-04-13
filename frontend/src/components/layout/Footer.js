8177f58b37c2ca06b9147e7333a416b29a03f7c2
Sami Warraich <samiwarraich51@gmail.com>
2023-04-13 22:02:08 +0300

    fixes footer

--- frontend/src/components/layout/Footer.js
+++ frontend/src/components/layout/Footer.js
@@ -5,7 +5,7 @@ const Footer = () => {
     <>
       <footer className="py-1">
         <p className="text-center mt-1">
-          Shopping Cart - 2019-2020, All Rights Reserved
+          ShopIT - 2019-2023 All Rights Reserved
         </p>
       </footer>
     </>
